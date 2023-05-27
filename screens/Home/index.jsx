5e1a0510d13d1d91392b9ea068e690fd389cef8d
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2023-05-27 23:21:05 +0700

    Update index.jsx

--- screens/Home/index.jsx
+++ screens/Home/index.jsx
@@ -6,18 +6,20 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function({ navigation }){
   return (
     <SafeAreaView className="bg-white">
-      <TouchableOpacity className="p-2 m-2 bg-dark-purple"
-      onPress={() => navigation.navigate("RouteInfo")}>
-        <Text className="text-white">Thông tin tuyến</Text>
-      </TouchableOpacity>
-      <TouchableOpacity className="p-2 m-2 bg-dark-purple"
-      onPress={() => navigation.navigate("BusInfo")}>
-        <Text className="text-white">Thông tin xe</Text>
-      </TouchableOpacity>
-      <TouchableOpacity className="p-2 m-2 bg-dark-purple"
-      onPress={() => navigation.navigate("Address")}>
-        <Text className="text-white">Address screen</Text>
-      </TouchableOpacity>
+      <View className="flex flex-row">
+        <TouchableOpacity className="p-2 m-2 bg-dark-purple"
+        onPress={() => navigation.navigate("RouteInfo")}>
+          <Text className="text-white">Thông tin tuyến</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="p-2 m-2 bg-dark-purple"
+        onPress={() => navigation.navigate("BusInfo")}>
+          <Text className="text-white">Thông tin xe</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="p-2 m-2 bg-dark-purple"
+        onPress={() => navigation.navigate("Address")}>
+          <Text className="text-white">Address screen</Text>
+        </TouchableOpacity>
+      </View>
       <BottomBar className="bg-white"/>
     </SafeAreaView>
   )
