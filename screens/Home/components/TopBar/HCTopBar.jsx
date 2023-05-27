1060a15ca409516e3d4d7397ea6a4502ad79eede
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2023-05-27 20:21:34 +0700

    update screens

---
+++ screens/Home/components/TopBar/HCTopBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HCTopBar() {
+  return (
+    <div>HCTopBar</div>
+  )
+}
+
+export default HCTopBar
---
+++ screens/Home/index.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import { View } from "react-native";
+import BottomBar from "./components/BottomBar";
+
+export default function(){
+  return (
+    <View className="bg-white">
+      <BottomBar />
+    </View>
+  )
+};
