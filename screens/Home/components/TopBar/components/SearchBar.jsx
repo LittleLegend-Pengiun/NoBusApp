e6c6b287fd5e448a00cbf02d7d9c6ea83b117c32
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2023-05-28 13:56:09 +0700

    New icon for home

--- screens/Home/components/TopBar/components/SearchBar.jsx
+++ screens/Home/components/TopBar/components/SearchBar.jsx
@@ -5,7 +5,7 @@ import AntDesign from 'react-native-vector-icons/AntDesign'
 export default function() {
   return (
     <View className="absolute top-28 z-50 w-full">
-        <View className="w-11/12 items-center flex-row space-x-2 flex-1 bg-white p-2 self-center border border-black rounded-lg drop-shadow-md">
+        <View className="w-11/12 items-center flex-row space-x-2 flex-1 bg-white p-2 self-center border border-purple rounded-lg drop-shadow-lg">
           <AntDesign name="search1" size={20} color="gray"/>
           <TextInput 
            placeholder='Tìm kiếm địa điểm'
