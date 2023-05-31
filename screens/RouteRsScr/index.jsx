bb3dbd59defd76210c20d45bbf442249cbd32920
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2023-05-31 19:48:06 +0700

    Update index.jsx

--- screens/RouteRsScr/index.jsx
+++ screens/RouteRsScr/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Text, View, Image, TouchableOpacity, Button, StyleSheet } from 'react-native'
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View } from 'react-native'
 import PageTitle from '../components/PageTitle';
 import AddressInput from '../components/AddressInput';
 import InfoTab from './components/InfoTab';
